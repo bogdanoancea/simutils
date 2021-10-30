332893dce0aa883d0e3a60ee5c885e91a254969c
david-salgado <david.salgado.fernandez@gmail.com>
2021-10-30 11:21:17 +0200

    Write validate_csv and read_csv functions

---
+++ R/validate_csv.R
@@ -0,0 +1,57 @@
+#' @title Validate a csv file using an xml file as schema.
+#' 
+#' @description \code{validate_csv} validates a csv file using an xml file as 
+#' schema, i.e. specifying its column names and value ranges (not yet 
+#' implemented).
+#' 
+#' 
+#' @param xmlFileName File name of the xml file containing the schema for the 
+#' csv file.
+#' 
+#' @param csvFileName File name of the csv file to validate.
+#' 
+#' 
+#' @details Return \code{TRUE} if the csv file is validated, otherwise, it 
+#' returns \code{FALSE}. 
+#' 
+#' @examples 
+#' rootPath <- file.path(system.file(package = "simutils"), 'extdata')  
+#' 
+#' # antennas file
+#' csv_fn <- file.path(rootPath, "output_files", "antennas.csv")
+#' xml_fn <- file.path(rootPath, "metadata/output_files", "antennas_dict.xml")
+#' validate_csv(xml_fn, csv_fn)
+#'  
+#' @export
+validate_csv <- function(xmlFileName, csvFileName) {
+  
+  xml <- read_xml(xmlFileName)
+  xml.list <- as_list(xml)[[1]]
+
+  antennas_fn <- xml.list$specs_file$fileName[[1]]
+
+  if (basename(csvFileName) != antennas_fn) {
+    
+    stop('[simviz::validate_csv] The name of the csv file is not valid.')
+  }
+
+  colnames_xml <- unlist(purrr::map(xml.list, function(x){x[grep('ColName', names(x))]}))
+  specnames_xml <- vapply(strsplit(names(colnames_xml), '.', fixed = TRUE), `[`, 1, FUN.VALUE = character(1))
+  names(colnames_xml) <- specnames_xml
+  colnames_csv <- names(fread(csvFileName, nrows = 0))
+  invalid_colnames <- colnames_csv[!colnames_csv %in% colnames_xml] 
+  if (length(invalid_colnames) > 0) {
+    
+    stop(paste0('[simviz::validate_csv] Column names ', 
+                paste0(invalid_colnames, collapse = ', '),
+                 ' are not valid column names.'))
+    
+  }
+  
+  cat('[simviz::validate_csv] The column names are valid.\n')
+  return(TRUE)
+  
+  # We should consider the possibility of specifying a range element for each 
+  # variable in the xml file and check these ranges for each variable
+  
+}
