16685670a27da9b312810497913685c6dded7c24
david-salgado <david.salgado.fernandez@gmail.com>
2021-11-17 09:25:46 +0100

    Drop out exe file

--- R/runExeFile.R
+++ R/runExeFile.R
@@ -4,7 +4,14 @@
 #'  file of the simulator of mobile network event data and the input file 
 #' parameters to execute the simulator.exe from this path.
 #'  
-#' If the file doesn't exist, it returns an error.
+#' If the executable file doesn't exist, it returns an error.
+#' 
+#' The executable file must be download from the Assets section of this URL
+#' in github.com:
+#' 
+#' https://github.com/bogdanoancea/simulator/releases/tag/1.2.0
+#' 
+#' The name of the executable file is \code{simulator.exe}.
 #' 
 #' @param path_to_exe character vector with the absolute path to the executable 
 #' file of the simulator of mobile network event data.
@@ -20,15 +27,18 @@
 #' 
 #' @param antennasCFGFile name of the antennas file (input file).
 #' 
-#' @param outputFolder Absolute path where the output files are going to be saved.
+#' @param outputFolder Absolute path where the output files are going to be 
+#' saved.
 #' 
 #' @details Return invisible \code{NULL} after placing output files of the 
 #' simulation in the output folder.
 #' 
 #' @examples
+#' # exe file dowloaded into the R_USER folder:
+#' local_exe<- file.path(Sys.getenv('R_USER'), 'simulator.exe')
 #' rootPath <- system.file(package = "simutils")
 #' runExeFile(
-#'  path_to_exe       = file.path(rootPath, 'bin'),
+#'  path_to_exe       = dirname(local_exe),
 #'  input_folder      = file.path(rootPath, "extdata/input_files"),
 #'  simulationCFGFile = "simulation.xml",
 #'  mapFile           = "map.wkt",
@@ -68,6 +78,8 @@ runExeFile <- function(
 		stop(paste0("[simutils::runExeFile] simulator.exe file cannot be found at ", path_to_exe, "\n"))
 	
 	}
+	
+	
 	# Execute the simulator
 	if (sysinfo['sysname'] == 'Windows') {
 	  
@@ -100,6 +112,10 @@ runExeFile <- function(
 			)
 		)
 	}
+	
+	cat(paste0('[simutils::runExeFile] output written in ', 
+	           file.path(getwd(), outputFolder)))
+	
 	return(invisible(NULL))
 	
 }
